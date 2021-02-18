d2c84859671dd025ba4aa8e3c79839c6d1579885
daria <kozina.flowers@yandex.ru>
2021-02-18 23:57:03 +0300

    Lesson 4. Methods

--- src/geekbrains/src/ru/geekbrains/lesson3/Main.java
+++ src/geekbrains/src/ru/geekbrains/lesson3/Main.java
@@ -3,12 +3,16 @@ package ru.geekbrains.lesson3;
 import java.util.Scanner;
 
 public class Main {
-
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Your task is to guess the number");
         int range = 10;
         int number = (int) (Math.random() * range);
+        PlayLevel (range, number);
+        scanner.close();
+    }
+    private static void PlayLevel (int range, int number) {
         while(true){
             System.out.println("Guess the number from 0 to " + range);
             int input_number = scanner.nextInt();
@@ -21,6 +25,5 @@ public class Main {
                 System.out.println("The original number is biggerr");
             }
         }
-        scanner.close();
     }
 }
