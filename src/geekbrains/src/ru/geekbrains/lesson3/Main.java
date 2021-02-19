e8c629179161e20dc819b51795de5088129c50dd
daria <kozina.flowers@yandex.ru>
2021-02-19 13:09:26 +0300

    Lesson 5. Cycle For

--- src/geekbrains/src/ru/geekbrains/lesson3/Main.java
+++ src/geekbrains/src/ru/geekbrains/lesson3/Main.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
         System.out.println("Your task is to guess the number");
-        int range = 10;
-        int number = (int) (Math.random() * range);
-        PlayLevel (range, number);
+        for (int i = 10; i <= 30; i += 10) PlayLevel (i);
+        System.out.println("You won!!!r");
         scanner.close();
     }
-    private static void PlayLevel (int range, int number) {
+    private static void PlayLevel (int range) {
+        int number = (int) (Math.random() * range);
         while(true){
             System.out.println("Guess the number from 0 to " + range);
             int input_number = scanner.nextInt();
